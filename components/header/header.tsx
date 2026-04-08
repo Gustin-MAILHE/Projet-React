2744fcb3d1127cb5c9a0d2ad5c3b5f10a2765478
Gustin MAILHÉ <gustin.mailhe@etu.univ-amu.fr>
2026-04-08 19:04:13 +0200

    ajout logo dans header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,15 @@
 import {View, Text, Image} from "react-native";
 import { styles } from "./style";
+import React from "react";
 
 export const Header = () => {
 
     return (
         <View style={ styles.main }>
+            <Image
+                style={{ width: 80, height: 80, marginLeft: 32 }}
+                source={require("@/assets/images/logo.png")}
+            />
             <Text style={styles.text}>le logo</Text>
         </View>
     )
