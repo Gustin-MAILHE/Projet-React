1f16c55735b6b29980bf057fe177e494cc373949
Gustin MAILHÉ <gustin.mailhe@etu.univ-amu.fr>
2026-04-08 19:23:35 +0200

    ajout, retour home via logo

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,16 +1,20 @@
-import {View, Text, Image} from "react-native";
+import {View, Text, Image, TouchableOpacity} from "react-native";
 import { styles } from "./style";
 import React from "react";
+import {router} from "expo-router";
 
 export const Header = () => {
 
     return (
         <View style={ styles.main }>
-            <Image
-                style={{ width: 80, height: 80, marginLeft: 32 }}
-                source={require("@/assets/images/logo.png")}
-            />
-            <Text style={styles.text}>le logo</Text>
+            <TouchableOpacity
+                onPress={ ()=> router.push("/") }
+            >
+                <Image
+                    style={{ width: 80, height: 80, marginLeft: 32 }}
+                    source={require("@/assets/images/logo.png")}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
