16cf75afc9550acdd7ff1a05e8a8baca87ef2cee
TCG-Blob <oriane.mejean@etu.univ-amu.fr>
2026-04-08 18:12:33 +0200

    commencement du style

--- components/sequence_memory/SequenceMemory.jsx
+++ components/sequence_memory/SequenceMemory.jsx
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#121212',
+    backgroundColor: '#151525',
   },
   headerText: {
     fontSize: 32,
